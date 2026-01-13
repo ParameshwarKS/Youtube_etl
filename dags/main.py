08d132cef3268c3f05bd2a8df09c45d2257d2f6f
unknown <kaveti.parameshwar123@gmail.com>
2026-01-13 23:22:00 +0530

    changes in main.py

--- dags/main.py
+++ dags/main.py
@@ -53,6 +53,8 @@ with DAG(
     trigger_update_db = TriggerDagRunOperator(
         task_id="trigger_update_db",
         trigger_dag_id="update_db",
+        reset_dag_run=True,
+        wait_for_completion=False,
     )
 
     # Define dependencies
@@ -74,6 +76,8 @@ with DAG(
     trigger_data_quality = TriggerDagRunOperator(
         task_id="trigger_data_quality",
         trigger_dag_id="data_quality",
+        reset_dag_run=True,
+        wait_for_completion=False,
     )
 
     # Define dependencies
